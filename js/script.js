86529b1a5ff0e0177b5b11f39cc1c947cd5cb5da
Cristopher Ferrari <ferraricristopher93@gmail.com>
2023-12-11 15:15:25 +0100

    JS; remake return games

--- js/script.js
+++ js/script.js
@@ -30,33 +30,30 @@ createApp ({
     data() {
         return {
             activeItem : 0,
-            images : [
-                {
-                    image: 'img/01.webp',
-                    title: 'Marvel\'s Spiderman Miles Morales',
-                    description: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.'
-                },
-                {
-                    image: 'img/02.webp',
-                    title: 'Ratchet & Clank: Rift Apart',
-                    description: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.'
-                },
-                {
-                    image: 'img/03.webp',
-                    title: 'Fortnite',
-                    description: 'Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.'
-                },
-                {
-                    image: 'img/04.webp',
-                    title: 'Stray',
-                    description: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city'
-                },
-                {
-                    image: 'img/05.webp',
-                    title: 'Marvel\'s Avengers',
-                    description: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.'
-                }
-            ]
+            game : {
+                images : [
+                    'img/01.webp',
+                    'img/02.webp',
+                    'img/03.webp',
+                    'img/04.webp',
+                    'img/05.webp'
+                ],
+                description : [
+                    'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                    'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                    'Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.',
+                    'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                    'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay'
+                ],
+                title : [
+                    'Marvel\'s Spiderman Miles Morales',
+                    'Ratchet & Clank: Rift Apart',
+                    'Fortnite',
+                    'Stray',
+                    'Marvel\s Avengers'
+                ],
+
+            }
         }
     }
 }).mount('#app');
